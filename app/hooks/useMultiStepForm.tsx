5489cabf1dd6bf8bcd75ee0a95ba305f69406ed5
Orion Gabriel <orionesu.dev@gmail.com>
2024-12-25 01:37:01 +0100

    add custom hook for multi-step form navigation

---
+++ app/hooks/useMultiStepForm.tsx
@@ -0,0 +1,65 @@
+import { FormData } from "@/components/form/MultiStepForm";
+import React from "react";
+import { ReactElement } from "react";
+import { UseFormTrigger } from "react-hook-form";
+
+export function useMultiStepForm(
+	steps: ReactElement[],
+	trigger: UseFormTrigger<FormData>
+) {
+	const [currentStepIndex, setCurrentStepIndex] = React.useState(0);
+
+	async function next() {
+		const currentStep = currentStepIndex + 1;
+		const fields =
+			currentStep === 1
+				? ["items"]
+				: currentStep === 2
+				? ["street", "city", "zipCode"]
+				: ["username", "password"];
+
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		const isStepValid = await trigger(fields);
+
+		if (!isStepValid) return;
+		setCurrentStepIndex((prev) => {
+			if (prev >= steps.length - 1) return prev;
+
+			return prev + 1;
+		});
+
+		// window.scrollTo({
+		// 	top: 0,
+		// 	left: 0,
+		// 	behavior: "smooth",
+		// });
+	}
+
+	function back() {
+		setCurrentStepIndex((prev) => {
+			if (prev <= steps.length - 1) return prev;
+
+			return prev - 1;
+		});
+	}
+
+	function cancel() {
+		setCurrentStepIndex(0);
+	}
+
+	function goTo(index: number) {
+		setCurrentStepIndex(index);
+	}
+
+	return {
+		currentStepIndex,
+		step: steps[currentStepIndex],
+		steps,
+		isLastStep: currentStepIndex === steps.length - 1,
+		goTo,
+		back,
+		next,
+		cancel,
+	};
+}
